156e7e0f9463300e580471b7f3e7d90c08cb2faf
parelkobra <ruiz.6789@gmail.com>
2020-10-12 00:13:08 +0200

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const modalObserver = new MutationObserver(mutationList => {
+  for (const mutation of mutationList) {
+    const addedNode = mutation.addedNodes[0];
+    if (addedNode !== undefined && addedNode.hasAttribute('role')) {
+      if (addedNode.attributes.role.value == 'presentation') {
+        addedNode.parentNode.removeChild(addedNode);
+        break;
+      }
+    }
+  }
+});
+
+const scrollObserver = new MutationObserver(mutationList => {
+  for (const mutation of mutationList) {
+    mutation.target.style.overflow = "initial";
+  }
+});
+
+modalObserver.observe(document.body, {
+  childList: true
+});
+
+scrollObserver.observe(document.body, {
+  attributes: true
+});
